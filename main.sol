e6e1c7b4f887b1aecceee384cf4513ea97b87392
ayushi12ayush <50498938+ayushi12ayush@users.noreply.github.com>
2020-08-24 08:42:29 +0530

    Update main.sol

--- main.sol
+++ main.sol
@@ -1,16 +1,21 @@
 pragma solidity ^0.4.0;
+//our contract
 contract splitit{
 address[] employees=[xxx,xx,yy,zz,kk];
 mapping(address->unit) withdrawnAmounts;
 unit totalrecieved=0;
+
 function splitit() payable{
 updatetotalrecieved();
 }
+
 function() payable{
 updatetotalrecieved();}
+
 function updatetoatlrecieved() internal{
 totalrecieved+=msg.value;
 }
+
 modifier canwithdraw(){
 bool contains=false;
 for(uinti=0;i<employees.length;i++){
@@ -20,6 +25,7 @@ contains=true;}
 require(contains);
 -;
 }
+
 function withdraw() canWithdraw{
 unit amountallocated=totalrecieved/emplotyee.length;
 unit  amountwithdrawn= withdrawnAmounts[msg.sender];
@@ -27,4 +33,5 @@ unit amount=amountallocated-amountwithdrawn;
 withdrawnAmounts[msg.sender]=amountwithdrawn+amount;
 if(amount>0){
 msg.sender.transfer(amount);}
+}
 }
